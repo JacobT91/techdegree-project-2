1e43006fe1ad5f5e491edda0e4bb4c6ddc9d63e3
Jacob Thompson <jacob91.work@gmail.com>
2020-06-14 16:56:10 -0600

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,21 @@ players = copy.deepcopy(constants.PLAYERS)
 
 
 def clean_data():
-    for data in players:
-        print(int(data['height'].replace('inches', '')))
+    #Loops through list of dict
+    for player in players:
+        #Replace letter charactors with empty string value
+        h_task = player['height'].replace(' inches', '')
+        
+        #If, El, creates a bool value from a key value
+        if player['experience'] == 'YES':
+            bool_task = bool('TRUE')
+
+        else:
+            bool_task = bool()
+        
+
+        print(int(h_task), bool_task)
+
         
 
 
