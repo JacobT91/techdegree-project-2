d7f6a63fc1ffa556c5b804dca5138ae8f181de90
Jacob Thompson <jacob91.work@gmail.com>
2020-06-17 18:47:41 -0600

    Delete practice.py

--- practice.py
+++
@@ -1,8 +0,0 @@
-rows = range(4)
-cols = range(10)
-
-print([(x, y) for y in rows for x in cols])
-
-print([(letter, number) for number in range(1, 5) for letter in 'abc'])
-
-print({number: letter for letter, number in zip('abcdefghijklmnopqrstuvwxyz', range(1, 27))})
