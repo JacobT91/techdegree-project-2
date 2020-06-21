46ad3cac28bf00584940cb6634aa2504fcf2fed7
Jacob Thompson <jacob91.work@gmail.com>
2020-06-20 20:28:34 -0600

    Update

--- app.py
+++ app.py
@@ -4,9 +4,11 @@ import random
 
 teams = copy.deepcopy(constants.TEAMS)
 players = copy.deepcopy(constants.PLAYERS)
-
 num_players_team = int(round(len(constants.PLAYERS) / len(constants.TEAMS)))
+
+# This line creates a teams dict()
 teams_dict = dict.fromkeys(teams, [])
+
 experienced = []
 inexperienced = []
 
@@ -19,56 +21,10 @@ def clean_data():
         experienced.append(i) if i['experience'] == True else inexperienced.append(i)
         
 
+# Please help me write this function. I was told I have to use the teams list to create either a dict() or list()
 def balance_teams():
-    for v in teams_dict.values():
-        exper_rand_player = random.choice(experienced)
-        v.append(exper_rand_player)
-        experienced.remove(exper_rand_player)
-        inexper_rand_player = random.choice(inexperienced)
-        v.append(inexper_rand_player)
-        inexperienced.remove(inexper_rand_player)
-
-        if len(v) == num_players_team:
-            break
-        
-
-def start_tool():
-    print('\nBASKETBALL TEAM STATS TOOL\n')
-    print('------- MENU -------')
-    print(
-        '\nHere are your choices:\n',
-        '1) Display Team Stats\n',
-        '2) Quit\n'
-        )
-    
-    menu_input = int(input('Enter an option > '))
-    print()
-
-    if menu_input == 1:
-        for num, i in enumerate(teams_dict, 1):
-            print(f'{num}) {i}')
-
-    menu_input = int(input('\nEnter an option > '))
-    print()
-    
 
 
-
-# def balance_teams(teams_list):
-    # clean_teams = {k: v for k, v in enumerate(teams, 1)}
-    # teams_dict.update(clean_teams)
-    
-    # for k, v in teams_dict:
-
-    
-        # if len(i) == num_players_team:
-        # rand_player = random.choice(experienced)
-        # i.update(rand_player)
-        # experienced.remove(rand_player)
-            # rand_player2 = random.choice(inexperienced)
-            # i.update(rand_player2)
-            # inexperienced.remove(rand_player2)
-        
 
 
         
@@ -80,5 +36,4 @@ def start_tool():
 if __name__ == "__main__":
     clean_data()
     balance_teams()
-    start_tool()
-    
+
--- draft_app.py
+++ draft_app.py
@@ -8,27 +8,26 @@ players = copy.deepcopy(constants.PLAYERS)
 experienced = []
 inexperienced = []
 
+panthers = []
+bandits = []
+warriors = []
 
 
-
-# def display_teams():
-#     for i, team in enumerate(teams, 1):
-        
-#         print(bal)
-        
-        
-# display_teams()
+# Reformats players height, guardians, and experience from players list
 def clean_data():
     for player in players:
+        # Replaces letter characters with empty string value
         player['height'] = player['height'].replace(' inches', '')
         player['height'] = int(player['height'])
-        player['experience'] = bool("TRUE")\
-            if player['experience'] == 'YES' else bool()
+        # If, Else, creates a bool value from a key
+        # value and adds players to a list
+        player['experience'] = bool("TRUE") if player['experience'] == 'YES' else bool()
         experienced.append(player) if player['experience'] == bool('TRUE') else inexperienced.append(player)
+        # Split up the guardian string into a List
         player['guardians'] = player['guardians'].split(' and ')
 
 
-# # Selects players randomly by experience bool statement
+# Selects players randomly by experience bool statement
 def random_player(x_team):
     rand_player = random.choice(experienced)
     x_team.append(rand_player)
@@ -38,97 +37,97 @@ def random_player(x_team):
     inexperienced.remove(rand_player2)
 
 
-# # Assigns random players to specified teams
-# def balance_team(p_team, b_team, w_team):
-#     while len(experienced) and len(inexperienced) != 0:
-#         random_player(p_team)
-#         random_player(b_team)
-#         random_player(w_team)
+# Assigns random players to specified teams
+def balance_team(p_team, b_team, w_team):
+    while len(experienced) and len(inexperienced) != 0:
+        random_player(p_team)
+        random_player(b_team)
+        random_player(w_team)
 
 
-# def stats_tool():
-#     print('\nBASKETBALL TEAM STATS TOOL')
+def stats_tool():
+    print('\nBASKETBALL TEAM STATS TOOL')
 
-#     while True:
-#         print('\n\n------- MENU -------\n')
-#         print('\nHere are you choices:\n1) Display team stats\n2) Quit\n')
+    while True:
+        print('\n\n------- MENU -------\n')
+        print('\nHere are you choices:\n1) Display team stats\n2) Quit\n')
 
-#         while True:
-#             try:
-#                 menu_opt = int(input('Enter an option > '))
-#                 if menu_opt < 1 or menu_opt > 2:
-#                     raise ValueError('That value is outside of the range.')
+        while True:
+            try:
+                menu_opt = int(input('Enter an option > '))
+                if menu_opt < 1 or menu_opt > 2:
+                    raise ValueError('That value is outside of the range.')
 
-#             except ValueError as err:
-#                 print('\n\n({})'.format(err))
-#                 print('Your options are > [1 - 2]\n')
-#                 continue
+            except ValueError as err:
+                print('\n\n({})'.format(err))
+                print('Your options are > [1 - 2]\n')
+                continue
 
-#             else:
-#                 if menu_opt == 2:
-#                     print('\nQuitting...\n')
-#                     exit()
+            else:
+                if menu_opt == 2:
+                    print('\nQuitting...\n')
+                    exit()
 
-#                 elif menu_opt == 1:
-#                     print(
-#                         '\nPick your team:\n'
-#                         '1) Panthers\n'
-#                         '2) Bandits\n'
-#                         '3) Warriors\n'
-#                     )
-#             while True:
-#                 try:
-#                     menu_opt = int(input('Enter an option > '))
-#                     if menu_opt < 1 or menu_opt > 3:
-#                         raise ValueError('That value is outside of the range.')
+                elif menu_opt == 1:
+                    print(
+                        '\nPick your team:\n'
+                        '1) Panthers\n'
+                        '2) Bandits\n'
+                        '3) Warriors\n'
+                    )
+            while True:
+                try:
+                    menu_opt = int(input('Enter an option > '))
+                    if menu_opt < 1 or menu_opt > 3:
+                        raise ValueError('That value is outside of the range.')
 
-#                 except ValueError as err:
-#                     print('\n\n{}'.format(err))
-#                     print('Your options are > [1 - 3]\n\n')\
+                except ValueError as err:
+                    print('\n\n{}'.format(err))
+                    print('Your options are > [1 - 3]\n\n')\
 
-#                 else:
-#                     if menu_opt == 1:
-#                         input_team = panthers
-#                         print('\nTeam: Panthers Stats')
+                else:
+                    if menu_opt == 1:
+                        input_team = panthers
+                        print('\nTeam: Panthers Stats')
 
-#                     elif menu_opt == 2:
-#                         input_team = bandits
-#                         print('\nTeam: Bandits Stats')
+                    elif menu_opt == 2:
+                        input_team = bandits
+                        print('\nTeam: Bandits Stats')
 
-#                     elif menu_opt == 3:
-#                         input_team = warriors
-#                         print('\nTeam: Warriors Stats')
+                    elif menu_opt == 3:
+                        input_team = warriors
+                        print('\nTeam: Warriors Stats')
 
-#                     team_exper = int()
-#                     team_inexper = int()
-#                     sep = ', '
+                    team_exper = int()
+                    team_inexper = int()
+                    sep = ', '
 
-#                     for i in input_team:
-#                         if i['experience'] == bool('TRUE'):
-#                             team_exper += 1
+                    for i in input_team:
+                        if i['experience'] == bool('TRUE'):
+                            team_exper += 1
 
-#                         elif i['experience'] == bool():
-#                             team_inexper += 1
+                        elif i['experience'] == bool():
+                            team_inexper += 1
 
-#                     team_height = [i['height'] for i in input_team]
-#                     team_names = [i['name'] for i in input_team]
-#                     team_guard = [sep.join(i['guardians']) for i in input_team]
-#                     team_names = sep.join(team_names)
+                    team_height = [i['height'] for i in input_team]
+                    team_names = [i['name'] for i in input_team]
+                    team_guard = [sep.join(i['guardians']) for i in input_team]
+                    team_names = sep.join(team_names)
 
-#                     print('-' * 20)
-#                     print('Total players: {}'.format(len(input_team)))
-#                     print('Total experienced: {}'.format(team_exper))
-#                     print('Total inexperienced: {}'.format(team_inexper))
-#                     print('Average height: {}'.format(round(sum(team_height) /
-#                           len(team_height), 1))
-#                           )
-#                     print('\n\nPlayers on Team:\n  {}.\n\n'.format(team_names))
-#                     print('Guardians:\n  {}.\n\n'.format(sep.join(team_guard)))
-#                     input("Press [ENTER] to continue...")
-#                     break
-#             break
+                    print('-' * 20)
+                    print('Total players: {}'.format(len(input_team)))
+                    print('Total experienced: {}'.format(team_exper))
+                    print('Total inexperienced: {}'.format(team_inexper))
+                    print('Average height: {}'.format(round(sum(team_height) /
+                          len(team_height), 1))
+                          )
+                    print('\n\nPlayers on Team:\n  {}.\n\n'.format(team_names))
+                    print('Guardians:\n  {}.\n\n'.format(sep.join(team_guard)))
+                    input("Press [ENTER] to continue...")
+                    break
+            break
 
-# if __name__ == "__main__":
-#     clean_data()
-#     balance_team(panthers, bandits, warriors)
-#     stats_tool()
+if __name__ == "__main__":
+    clean_data()
+    balance_team(panthers, bandits, warriors)
+    stats_tool()
