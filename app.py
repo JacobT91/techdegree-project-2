949f9e4da5b420ab12b5a6db166440cd85abc173
Jacob Thompson <jacob91.work@gmail.com>
2020-06-23 17:50:06 -0600

    Completed

--- app.py
+++ app.py
@@ -37,82 +37,87 @@ def balance_teams(team_list):
             team_list[num]['players'].append(inexperienced.pop())
 
 
+def stats_tool():
+    sep = ', '
+    print('\nBASKETBALL TEAM STATS TOOL\n')
+    print('---- MENU----\n')
+    while True:
+        print(
+        'Here are your choices:\n'
+        '1) Display Team Stats\n'
+        '2) Quit\n'
+        )
 
-
-
-# def stats_tool():
-#     sep = ', '
-#     print('\nBASKETBALL TEAM STATS TOOL\n')
-#     print('---- MENU----\n')
-#     while True:
-#         print(
-#         'Here are your choices:\n'
-#         '1) Display Team Stats\n'
-#         '2) Quit\n'
-#         )
-#         menu_opt = int(input('Enter an option > '))
-#         print()
+        try:
+            menu_opt = int(input('Enter an option > '))
         
-#         if menu_opt == 2:
-#             print('Quiting...\n')
-#             exit()
-
-#         elif menu_opt == 1:
-#             for num, team in enumerate(teams, 1):
-#                 print(f'{num}) {team}')
-
-#             print()
-#             menu_opt = menu_opt = int(input('Enter an option > '))
-#             input_team = sorted_teams[menu_opt - 1]
-#             team_exper = 0
-#             team_inexper = 0
-#             team_names = [i['name'] for i in input_team]
-#             team_heights = [i['height'] for i in input_team]
-#             average_height = round(sum(team_heights) / len(input_team), 1)
-#             team_guard = [sep.join(i['guardians']) for i in input_team]
-#             for i in input_team:
-#                 if i['experience'] == bool('TRUE'):
-#                     team_exper += 1
-
-#                 elif i['experience'] == bool():
-#                     team_inexper += 1
-
-#             print()
-#             print(f'Team: {teams[menu_opt - 1]} stats')
-#             print('-' * 20)
-#             print(f'Total players: {len(input_team)}\n')
-#             print('Total experienced: {}'.format(team_exper))
-#             print('Total inexperienced: {}'.format(team_inexper))
-#             print('Average height: {}\n'.format(average_height))
-            
-#             print('Players on team:\n   {}\n'.format(sep.join(team_names)))
-#             print('Guardians:\n   {}\n'.format(team_guard))
-#             input('Press enter to continue... ')
-#             print()
-#             continue
-
-
-
-
-
-
+        except ValueError:
+            print('\nYour options are > 1 - 2./nPlease try agian...\n')
+            continue
         
+        print()
+        if menu_opt >= 3:
+            print('That is not a valid option. Please try agian...\n')
+            continue
         
-        
-        
+        elif menu_opt == 2:
+            print('Quiting...\n')
+            exit()
+
+        elif menu_opt == 1:
+            while True:
+                for num, team in enumerate(teams, 1):
+                    print(f'{num}) {team}')
+
+                print()
+
+                try:
+                    menu_opt = menu_opt = int(input('Enter an option > '))
+                
+                except ValueError:
+                    print('\nYour options are > 1 - 2./nPlease try agian...\n')
+                    continue
+
+                if menu_opt > len(teams):
+                    print('\nThat is not a valid option. Please try agian...\n')
+                    continue
+
+                if menu_opt <= len(teams):
+                    input_team = sorted_teams[menu_opt - 1]
+                    team_names = []
+                    team_heights = []
+                    team_exper = 0
+                    team_inexper = 0
+                    team_guard = []
+                    
+                    for num in range(0, len(input_team['players'])):
+                        team_names.append(input_team['players'][num]['name'])
+                        team_heights.append(input_team['players'][num]['height'])
+                        team_guard += input_team['players'][num]['guardians']
+                        
+                        if input_team['players'][num]['experience'] == bool('T'):
+                            team_exper += 1
+
+                        elif input_team['players'][num]['experience'] == bool():
+                            team_inexper += 1
+                    
+                    print()
+                    print(f'Team: {teams[menu_opt - 1]} stats')
+                    print('-' * 20)
+                    print(f'Total players: {len(team_names)}\n')
+                    print('Total experienced: {}'.format(team_exper))
+                    print('Total inexperienced: {}'.format(team_inexper))
+                    print('Average height: {}\n'.format(round(sum(team_heights)/ len(team_heights), 1)))
+                    
+                    print('Players on team:\n   {}\n'.format(sep.join(team_names)))
+                    print('Guardians:\n   {}\n'.format(sep.join(team_guard)))
+                    input('Press enter to continue... ')
+                    print()
+                    break
             
 
 if __name__ == "__main__":
     clean_data()
     sort_teams(sorted_teams)
     balance_teams(sorted_teams)
-    for name in sorted_teams[0]['players']:
-        print(name['name'])
-    print()
-    for name in sorted_teams[1]['players']:
-        print(name['name'])
-
-    # for num, team in enumerate(sorted_teams):
-    #     print(team[teams[num]][0])
-    # # stats_tool()
-    
+    stats_tool()
