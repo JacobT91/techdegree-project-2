5e85aae993a08c5e64c8420b2ca051838953c88c
Jacob Thompson <jacob91.work@gmail.com>
2020-06-23 18:06:08 -0600

    Submitted for review

--- app.py
+++ app.py
@@ -16,8 +16,9 @@ def clean_data():
         i['height'] = int(i['height'].replace(' inches', ''))
         i['experience'] = bool('T') if i['experience'] == 'YES' else bool()
         i['guardians'] = i['guardians'].split(' and ')
-        experienced.append(i) if i['experience'] == True else inexperienced.append(i)
-    
+        experienced.append(i) if i['experience'] == bool('T')\
+            else inexperienced.append(i)
+
 
 def sort_teams(team_list):
     for key in (teams):
@@ -26,7 +27,6 @@ def sort_teams(team_list):
             'players': []
         }
         team_list.append(add_team)
-        
     return team_list
 
 
@@ -43,23 +43,23 @@ def stats_tool():
     print('---- MENU----\n')
     while True:
         print(
-        'Here are your choices:\n'
-        '1) Display Team Stats\n'
-        '2) Quit\n'
+            'Here are your choices:\n'
+            '1) Display Team Stats\n'
+            '2) Quit\n'
         )
 
         try:
             menu_opt = int(input('Enter an option > '))
-        
+
         except ValueError:
-            print('\nYour options are > 1 - 2./nPlease try agian...\n')
+            print('\nYour options are > 1 - 2\nPlease try agian...\n')
             continue
-        
+
         print()
         if menu_opt >= 3:
             print('That is not a valid option. Please try agian...\n')
             continue
-        
+
         elif menu_opt == 2:
             print('Quiting...\n')
             exit()
@@ -73,13 +73,16 @@ def stats_tool():
 
                 try:
                     menu_opt = menu_opt = int(input('Enter an option > '))
-                
+
                 except ValueError:
-                    print('\nYour options are > 1 - 2./nPlease try agian...\n')
+                    print('\nYour options are > 1 - {}\nPlease try agian...\n'.format(len(teams)))
                     continue
 
                 if menu_opt > len(teams):
-                    print('\nThat is not a valid option. Please try agian...\n')
+                    print(
+                        '\nThat is not a valid option.'
+                        ' Please try agian...\n'
+                    )
                     continue
 
                 if menu_opt <= len(teams):
@@ -89,32 +92,42 @@ def stats_tool():
                     team_exper = 0
                     team_inexper = 0
                     team_guard = []
-                    
+
                     for num in range(0, len(input_team['players'])):
                         team_names.append(input_team['players'][num]['name'])
-                        team_heights.append(input_team['players'][num]['height'])
+                        team_heights.append(
+                            input_team['players'][num]['height']
+                        )
                         team_guard += input_team['players'][num]['guardians']
-                        
-                        if input_team['players'][num]['experience'] == bool('T'):
+
+                        if input_team['players'][num]['experience'] == \
+                                bool('T'):
                             team_exper += 1
 
-                        elif input_team['players'][num]['experience'] == bool():
+                        elif input_team['players'][num]['experience'] == \
+                                bool():
                             team_inexper += 1
-                    
+
                     print()
                     print(f'Team: {teams[menu_opt - 1]} stats')
                     print('-' * 20)
                     print(f'Total players: {len(team_names)}\n')
                     print('Total experienced: {}'.format(team_exper))
                     print('Total inexperienced: {}'.format(team_inexper))
-                    print('Average height: {}\n'.format(round(sum(team_heights)/ len(team_heights), 1)))
-                    
-                    print('Players on team:\n   {}\n'.format(sep.join(team_names)))
+                    print('Average height: {}\n'.format(
+                        round(sum(team_heights) / len(team_heights), 1)
+                        )
+                    )
+
+                    print('Players on team:\n   {}\n'.format(
+                        sep.join(team_names)
+                        )
+                    )
                     print('Guardians:\n   {}\n'.format(sep.join(team_guard)))
                     input('Press enter to continue... ')
                     print()
                     break
-            
+
 
 if __name__ == "__main__":
     clean_data()
